8c31c482d405a1cc27d156c8b44d382fd80a4c8f
Linh Ngô <ngo.linh@daihoclongan.edu.vn>
2026-05-31 19:31:02 +0700

    tailwind powershell: .\tailwindcss.exe -o ./tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // Yêu cầu Tailwind quét tất cả các file HTML và JS nằm cùng thư mục
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                // Khai báo lại mã màu chủ đạo của dự án
+                maroon: {
+                    DEFAULT: '#800000',
+                    dark: '#5a0000'
+                }
+            }
+        },
+    },
+    plugins: [],
+}
